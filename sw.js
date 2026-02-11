d2bddf96454d860f24e468c5db5c8c5caf5330a9
Prince146-cloud <97993149+Prince146-cloud@users.noreply.github.com>
2026-02-11 04:47:36 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
 // sw.js
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { title: 'Security Alert', body: 'Immediate action required!' };
-    
-    const options = {
-        body: data.body,
-        icon: 'https://cdn-icons-png.flaticon.com/512/564/564619.png', // A warning icon
-        vibrate: [200, 100, 200],
-        data: { url: 'https://google.com' } // Where you go if you click
-    };
-
+    const data = event.data ? event.data.json() : { title: 'Notification', body: 'New Message!' };
     event.waitUntil(
-        self.registration.showNotification(data.title, options)
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: 'https://supabase.com/favicons/favicon-32x32.png'
+        })
     );
-});
-
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    event.waitUntil(clients.openWindow(event.notification.data.url));
 });
